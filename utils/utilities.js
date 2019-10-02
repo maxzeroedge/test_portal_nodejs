4130f7bc76e82e82b8bb836c4730ddca1fc68088
pc <pc@pop-os.localdomain>
2019-10-02 18:35:16 +0530

    removed sanitizeData for now

--- utils/utilities.js
+++ utils/utilities.js
@@ -1,5 +1,6 @@
 const constants = require('./constants')
 const sanitizer = require('sanitize')()
+const emailPattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
 const respondBack = (attrs) => {
 	const {error, statusCode, body, headers, callback} = attrs
@@ -18,27 +19,27 @@ const respondBack = (attrs) => {
  */
 const sanitizeData = (data) => {
 	// TODO:
-	if(typeof data == 'object'){
-		if(Arrays.isArray(data)){
-			//
-		} else {
-			data = data.map((v, k)=>{
-				// Don't sanitize email or username
-				// TODO: Use /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-				// from https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
-				if(k == 'username'){
-					if(/[\w\d.]+@[\w\d.]+\.\w+/.test(v)){
-						return "";
-					}
-					return v;
-				}
-				return sanitizeData(v)
-			})
-		}
-	} else if(typeof data == 'string'){
-		// TODO:
-		data = sanitizer.value(data, 'string')
-	}
+	// if(typeof data == 'object'){
+	// 	if(Arrays.isArray(data)){
+	// 		//
+	// 	} else {
+	// 		data = data.map((v, k)=>{
+	// 			// Don't sanitize email or username
+	// 			// TODO: Use /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+	// 			// from https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+	// 			if(k == 'username'){
+	// 				if(emailPattern.test(v)){ // /[\w\d.]+@[\w\d.]+\.\w+/
+	// 					return "";
+	// 				}
+	// 				return v;
+	// 			}
+	// 			return sanitizeData(v)
+	// 		})
+	// 	}
+	// } else if(typeof data == 'string'){
+	// 	// TODO:
+	// 	data = sanitizer.value(data, 'string')
+	// }
 	return data
 }
 
