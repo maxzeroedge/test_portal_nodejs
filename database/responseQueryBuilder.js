cd7a31eeb3fb2c9c12bb209b5012bb5b5b196197
Palash <maxzeroedge@gmail.com>
2019-10-19 18:12:32 +0530

    updated response query builder

--- database/responseQueryBuilder.js
+++ database/responseQueryBuilder.js
@@ -3,32 +3,67 @@ const sanitizeData = require('../utils/utilities').sanitizeData
 const uuid = require('uuid/v4')
 const moment = require('moment')
 
-const updateAnswer = async(attrs) => {
+const getResponse = async(attrs) => {
+	let query = {
+		'TableName': process.env.RESPONSE_TABLE,
+		'IndexName': 'examIdUserIdIndex',
+		'ExpressionAttributeNames': {
+			'#examId': 'examId',
+			'#candidateId': 'candidateId'
+		},
+		'ExpressionAttributeValues': {
+			':examId': {
+				"S": attrs.examId
+			}, 
+			':candidateId': {
+				"S": attrs.candidateId
+			}
+		},
+		'KeyConditionExpression': '#examId = :examId and #candidateId = :candidateId'
+	}
+	const responseItem = await dbHandler.docClient.query(query).promise();
+	return responseItem.Items.length ? responseItem.Items[0] : false;
+}
+
+/**
+ * 
+ * @param {*} attrs {examId, candidateId, candidateResponse: {questionId, response, marks}} 
+ */
+const updateAnswer = async (attrs) => {
+	let responseItem = getResponse(attrs)
+	if(!responseItem){
+		responseItem = {
+			id: uuid(),
+			examId: attrs.examId,
+			candidateId: attrs.candidateId,
+			answerSheet: []
+		}
+	}
+	let candidateResponseIndex = -1;
+	let candidateResponse = attrs.candidateResponse || {}
+	responseItem.answerSheet.forEach((v,k)=>{
+		if(v.questionId == attrs.questionId){
+			candidateResponseIndex = k;
+			candidateResponse = v;
+		}
+	})
+	if(candidateResponseIndex > -1){
+		responseItem.answerSheet.splice(candidateResponseIndex, 1, candidateResponse)
+	}
 	let query = {
 		"TableName": process.env.RESPONSE_TABLE,
-		"Item": {
-			examId: attrs.examId,
-			questionId: attrs.questionId,
-			response: attrs.response
-		},
+		"Item": responseItem,
 		"ReturnValues": "ALL_NEW"
 	}
 	const response = await dbHandler.docClient.put(query).promise()
 	return response.Attributes
 }
-
-const updateEntry = async(attrs) => {
-	let query = {
-		"TableName": process.env.RESPONSE_TABLE,
-		"IndexName": "idIndex",
-		"Key": {
-			id: attrs.id
-		},
-		"ExpressionAttributeValues": {},
-		"UpdateExpression": "",
-		"ReturnValues": "ALL_NEW"
-	}
-	return await dbHandler.docClient.update(query).promise()
+/**
+ * 
+ * @param {*} attrs {examId, candidateId, candidateResponse: {questionId, response, marks}} 
+ */
+const updateEntry = async (attrs) => {
+	return await updateAnswer(attrs)
 }
 
 module.exports = {
